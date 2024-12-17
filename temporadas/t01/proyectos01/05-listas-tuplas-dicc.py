e580745405f7c97cbf4aa38e436478daca3b4f82
Juan <a23juanmg@iessanclemente.net>
2024-12-17 17:10:52 +0100

    Cambios menores

--- temporadas/t01/proyectos01/05-listas-tuplas-dicc.py
+++ temporadas/t01/proyectos01/05-listas-tuplas-dicc.py
@@ -80,3 +80,99 @@ print(nombre)
 print(edad)
 print(peso)
 print(hombre)
+
+
+#Diccionarios
+
+#Declaración
+miDiccionario = {
+    "Marca":"Ford",
+    "Modelo":"Fiesta",
+    "Año": 2002
+}
+print(miDiccionario['Modelo'])
+
+#Reasignar
+miDiccionario['Marca'] = 'Opel'
+print(miDiccionario)
+
+#Usar una tupla en un diccionario
+
+tuplaEnDic = {'nombre':'Alonso', 'Escudería':'Renault','Campeonatos':[2005,2006]}
+print(tuplaEnDic['Campeonatos'])
+
+#Quitar valores
+miDiccionario.pop('Modelo')
+print(miDiccionario)
+
+#Crear diccionario a partir de dos listas
+
+claves = ['Nombre','Apellido','Edad']
+valores = ['Perico','De los Palotes',33]
+datosUsuario = dict(zip(claves,valores))
+print(datosUsuario)
+
+#Comprobar si una clave está en un diccionario
+
+'Nombre' in datosUsuario
+
+#Imprime las claves del diccionario
+
+for x in datosUsuario:
+    print(datosUsuario[x])
+
+#Longitud
+
+print(len(datosUsuario))   
+
+#Eliminar ultimo elemento insertado
+
+coche = {
+    'marca':'Ford',
+    'modelo':'Mustang',
+    'año':1964
+}
+
+del coche['modelo']
+print(coche)
+
+#Clear vacía el diccionario
+
+coche.clear()
+print(coche)
+
+#Copiar diccionario
+
+copiaDiccionario = datosUsuario.copy()
+print(copiaDiccionario)
+
+#Diccionarios anidados
+
+pajaro1 = {
+    'nombre':'Arcadio',
+    'especie':'ninfa'
+}
+
+pajaro2 = {
+    'nombre':'Melquíades',
+    'especie':'ninfa'
+}
+pajaro3 = {
+    'nombre':'Jacinta',
+    'especie':'agaporni'
+}
+pajaro4 = {
+    'nombre':'Benito',
+    'especie':'agaporni'
+}
+
+mascotas = {
+    'Animal1':pajaro1,
+    'Animal2':pajaro2,
+    'Animal3':pajaro3,
+    'Animal4':pajaro4,
+}
+print(mascotas['Animal1'])
+print(mascotas['Animal2'])
+print(mascotas['Animal3'])
+print(mascotas['Animal4'])
