f3cf41fb553e6a7b7741dca9c8408486a9f09b6c
Juan <a23juanmg@iessanclemente.net>
2024-12-19 01:55:11 +0100

    Calculadora avanzada

--- temporadas/t01/proyectos02/08-calculadora_avanzada.py
+++ temporadas/t01/proyectos02/08-calculadora_avanzada.py
@@ -1,6 +1,19 @@
 """
 --- CALCULADORA AVANZADA ---
 
+Funcionamiento:
+
+Esta calculadora hace más operaciones que la básica, y a mayores almacena
+los resultados en un historial.
+
+1. Elegir una de las opciones del menú (Suma, resta, multiplicación, división,
+    potencia, raíz cuadrada, factorial, logaritmo o trigonometría)
+2. Introducir los números que vaya solicitando
+3. En el caso de la división, el divisor nunca puede ser cero.
+4. Tras introducir los números, veremos el resultado de la operación elegida.
+5. Con la opción "10" podemos ver el historial de operaciones realizadas.
+6. Para salir del programa hay que escoger "11" en el menú.
+
 """
 
 import math
@@ -15,7 +28,8 @@ def calculadora_avanzada():
         if not historial:
             print('\nEl historial vacío.')
         else:
-            print('\nHistorial de operaciones:\n')
+            print('\nHistorial de operaciones')
+            print('-------------------------\n')
             for i, operacion in enumerate(historial, start=1):
                 print(f'{i}. {operacion}')
 
@@ -58,7 +72,7 @@ def calculadora_avanzada():
             elif opcion == '6':
                 numero1 = float(input('\nRaíz cuadrada - Introduce el número: '))
                 resultado = math.sqrt(numero1)
-                linea_resultado = f'√{numero1} = {resultado}'
+                linea_resultado = f'√{numero1} = {resultado}\n'
                 imprimir_guardar(linea_resultado)
                 continue
                 
@@ -66,7 +80,7 @@ def calculadora_avanzada():
             elif opcion == '7':
                 numero1 = int(input('\nFactorial - Introduce un número entero: '))
                 resultado = math.factorial(numero1)
-                linea_resultado = f'{numero1}! = {resultado}'
+                linea_resultado = f'{numero1}! = {resultado}\n'
                 imprimir_guardar(linea_resultado)
                 continue
             
@@ -74,7 +88,7 @@ def calculadora_avanzada():
                 numero1 = float(input('\nLogaritmo - Introduce el número: '))
                 base = int(input('\nIntroduce la base (Por defecto 10): ') or 10)
                 resultado = math.log(numero1, base)
-                linea_resultado = f'log{base}({numero1}) = {resultado}'
+                linea_resultado = f'log{base}({numero1}) = {resultado}\n'
                 imprimir_guardar(linea_resultado)
 
             elif opcion == '9':
@@ -82,7 +96,7 @@ def calculadora_avanzada():
                 seno = math.sin(math.radians(angulo))
                 coseno = math.cos(math.radians(angulo))
                 tangente = math.tan(math.radians(angulo))
-                linea_resultado = f'Sen({angulo}°) = {seno}\nCos({angulo}°) = {coseno}\nTan({angulo}°) = {tangente}'
+                linea_resultado = f'Sen({angulo}°) = {seno}\nCos({angulo}°) = {coseno}\nTan({angulo}°) = {tangente}\n'
                 imprimir_guardar(linea_resultado)
                 continue
 
@@ -92,26 +106,26 @@ def calculadora_avanzada():
 
             if opcion == '1':
                 resultado = numero1 + numero2
-                linea_resultado = f'{numero1} + {numero2} = {resultado}'
+                linea_resultado = f'{numero1} + {numero2} = {resultado}\n'
                 imprimir_guardar(linea_resultado)
             elif opcion == '2':
                 resultado = numero1 - numero2
-                linea_resultado = f'{numero1} - {numero2} = {resultado}'
+                linea_resultado = f'{numero1} - {numero2} = {resultado}\n'
                 imprimir_guardar(linea_resultado)
             elif opcion == '3':
                 resultado = numero1 * numero2
-                linea_resultado = f'{numero1} x {numero2} = {resultado}'
+                linea_resultado = f'{numero1} x {numero2} = {resultado}\n'
                 imprimir_guardar(linea_resultado)
             elif opcion == '4':
                 if numero2 != 0:
                     resultado = numero1 / numero2
-                    linea_resultado = f'{numero1} / {numero2} = {resultado}'
+                    linea_resultado = f'{numero1} / {numero2} = {resultado}\n'
                     imprimir_guardar(linea_resultado)    
                 else:
                     print('\nError: No es posible dividir entre cero')
             elif opcion == '5':
                 resultado = math.pow(numero1,numero2)
-                linea_resultado = f'{numero1}^{numero2} = {resultado}'
+                linea_resultado = f'{numero1}^{numero2} = {resultado}\n'
                 imprimir_guardar(linea_resultado)
             
             
